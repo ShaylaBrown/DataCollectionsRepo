8fdd592dc878cba38d61e0414f8cbd488bb5edd3
Shayla Brown <shaylabrown227@gmail.com>
2024-04-16 11:35:49 -0500

    Refractured my original code

---
+++ Datacollections.js
@@ -0,0 +1,20 @@
+console.log("Data Collections!!")
+
+//My previous code:
+let cell1 = ("ID")
+let cell2 = ("Name")
+let cell3 = ("Ocupation")
+let cell4 = ("Age")
+
+console.log(cell1, cell2, cell3, cell4);
+let row1 = ("\n42, Bruce, Knight, 41")
+let row2 = ("\n57, Bob, Fry Cook, 19")
+let row3 = ("\n63, Blaine, Quiz Master, 58")
+let row4 = ("\n98, Bill, Doctor’s Assistant, 26")
+console.log(row1, row2, row3, row4)
+
+//Refractured Code
+let csvStr = ["ID, Name, Occupation, Age\n42, Bruce, Knight, 41\n57, Bob, Fry Cook, 19\n63, Blaine, Quiz Master, 58\n98, Bill,Doctor's Assistant, 26"]
+let table = csvStr.join(",");
+
+console.log(table);
