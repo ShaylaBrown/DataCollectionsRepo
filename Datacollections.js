f7726c41126d193d96b2c26d35cb2f90ab63031a
Shayla Brown <shaylabrown227@gmail.com>
2024-04-17 22:56:09 -0500

    done

--- Datacollections.js
+++ Datacollections.js
@@ -19,14 +19,45 @@ let table = csvStr.join(",");
 
 console.log(table);
 
-/////////
+///////
 const numColumns = ['ID', 'Name', 'Occupation', 'Age']
-const numColums2 = numColumns[0];
-console.log(numColumns.length);
-
 numColumns.length = 4
-console.log(numColumns);
+// console.log(numColumns);
+// const numColumns1 = ['42', 'Bruce', 'Knight', '41']
+// numColumns1.length = 4
+// console.log(numColumns1);
+// const numColumns2 = ['57', 'Bob', 'Fry Cook', '19']
+// numColumns2.length = 4
+// console.log(numColumns2);
+// const numColumns3 = ['63', 'Blaine', 'Quiz Master', '58']
+// numColumns3.length = 4
+// console.log(numColumns3);
+// const numColumns4 = ['98', 'Bill', 'Doctor\'s Assistant', '26']
+// numColumns4.length = 4
+// console.log(numColumns4);
 
-for (let i = 0; i < numColumns.length; i++) {
-    console.log(numColumns[i]);
-}
+let allcolumns =
+    [["ID", "Name", "Occupation", "Age"],
+    ["42", "Bruce", "Knight", "41"],
+    ["57", "Bob", "Fry Cook", "19"],
+    ["63", "Blaine", "Quiz Master", "58"],
+    ["98", "Bill", "Doctor’s Assistant", "26"]]
+
+    const objects = {
+        [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+        { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+        { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+        { id: "98", name: "Bill", occupation: "Doctor’s Assistant", age: "26" }]
+ }
+    console.log(objects); 
+
+    const vehicle = {
+        color: "blue",
+        hp: 4000,
+        year: 1989,
+        make: "Audi"
+    }
+    
+    for (const key in vehicle) {
+        console.log(vehicle[key]);
+    }
